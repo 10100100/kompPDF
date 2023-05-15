496a11c09ba9c63e2a7bbd25bc7e1997ad9ac8f1
10100100 <61347444+10100100@users.noreply.github.com>
2023-05-15 09:19:57 +0200

    Update kompPDF.sh

--- kompPDF.sh
+++ kompPDF.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 << 'Kommentar'
-    kompPDF - Komprimieren in Stapelverarbeitung für pdf Dateien
+    kompPDF - Komprimieren von pdf Dateien in Stapelverarbeitung 
     Copyright (C) 2023  André Dittmann
 
     This program is free software: you can redistribute it and/or modify
